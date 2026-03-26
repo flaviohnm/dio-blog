a9df591f515c16cb537d461a970789e60219a81c
Flavio Monteiro <flaviohnm@gmail.com>
2026-03-26 14:25:23 -0300

    :tada: init: first commit

---
+++ src/app.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+
+import click
+import sqlalchemy as sa
+from flask import Flask, current_app
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+migrate = Migrate()
+jwt = JWTManager()
+
+
+class Role(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    user: Mapped[list["User"]] = relationship(back_populates="role")
+
+    def __repr__(self) -> str:
+        return f"Role(id={self.id!r}, name={self.name!r})"
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(sa.String, nullable=False)
+    active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
+    role_id: Mapped[int] = mapped_column(sa.ForeignKey("role.id"))
+    role: Mapped["Role"] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
+
+@click.command("init-db")
+def init_db_command():
+
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo("Initialized the database.")
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="sqlite:///blog.sqlite",
+        JWT_SECRET_KEY="minha-senha-secreta-do-dio-bank-precisa-ter-mais-de-32-caracteres",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    app.cli.add_command(init_db_command)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    # register blueprint
+    from src.controllers import auth, role, user
+
+    app.register_blueprint(user.app)
+    app.register_blueprint(role.app)
+    app.register_blueprint(auth.app)
+
+    return app
---
+++ src/controllers/role.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from flask import Blueprint, request
+
+from src.app import Role, db
+
+app = Blueprint("role", __name__, url_prefix="/roles")
+
+
+@app.route("/", methods=["POST"])
+def create_role():
+    data = request.json
+    role = Role(name=data["name"])
+    db.session.add(role)
+    db.session.commit()
+    return {"message": "Role created!"}, HTTPStatus.CREATED
